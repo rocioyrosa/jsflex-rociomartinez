0b01de1355d17e39a7448f1d5310a330a4b2b299
rocioyrosa <rociomartinezcomunicacion@gmail.com>
2023-09-12 21:42:44 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Calculadora de costo de combustible para viaje
+
+function calcularCostoCombustible() {
+    let nuevoCalculo = true;
+
+    do {
+        let distanciaViaje = parseFloat(prompt("Ingresá la distancia de tu viaje (en kilómetros, utiliza solo números)"));
+        let precioCombustible = parseFloat(prompt("Ingresá el precio del litro de combustible (sin el signo $, solo números)"));
+        let rendimientoAuto = parseFloat(prompt("Ingresá el rendimiento de tu auto por litro (en kilómetros por litro, utiliza solo números)"));
+
+        if (!isNaN(distanciaViaje) && !isNaN(precioCombustible) && !isNaN(rendimientoAuto)) {
+            const litrosNecesarios = distanciaViaje / rendimientoAuto;
+            const costoTotal = litrosNecesarios * precioCombustible;
+
+            alert(`El costo total del combustible para tu viaje es de $${costoTotal}`);
+        } else {
+            alert("Para ver el costo total de tu viaje debes ingresar los valores en números (sin letras, ni signos)");
+        }
+
+        nuevoCalculo = confirm("¿Deseas calcular otro viaje?");
+    } while (nuevoCalculo);
+}
+
+calcularCostoCombustible();
