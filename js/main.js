b6d6485ab5603023ff312c6c58341658b2608bfa
rocioyrosa <rociomartinezcomunicacion@gmail.com>
2023-09-22 17:19:41 -0300

    Revert "array, for, cambio de nombre de variables, parseo de nros a float y tofixed(2)para resultados, cambio texto html"
    This reverts commit 015e6c33fddef9fc4a020b3b760aa21369aa252c.

--- js/main.js
+++ js/main.js
@@ -1,37 +1,24 @@
 // Calculadora de costo de combustible para viaje
 
 function calcularCostoCombustible() {
-    const viajes = [];
-    let continuar = true;  
+    let nuevoCalculo = true;
 
     do {
-        const distanciaViaje = parseInt(prompt("Ingresá la distancia de tu viaje en kilómetros (utilizá solo números)"));
-        const precioCombustible = parseFloat(prompt("Ingresá el precio del litro de combustible (sin el signo $, utiliza números decimales con punto)"));
-        const rendimientoAuto = parseFloat(prompt("Ingresá el rendimiento de kilómetros tu auto por litro (utilizá números decimales con punto)"));
+        let distanciaViaje = parseInt(prompt("Ingresá la distancia de tu viaje (en kilómetros, utiliza solo números)"));
+        let precioCombustible = parseInt(prompt("Ingresá el precio del litro de combustible (sin el signo $, solo números)"));
+        let rendimientoAuto = parseInt(prompt("Ingresá el rendimiento de tu auto por litro (en kilómetros por litro, utiliza solo números)"));
 
-        if (!isNaN(distanciaViaje) && !isNaN(precioCombustible) && !isNaN(rendimientoAuto) && rendimientoAuto !== 0) {
+        if (!isNaN(distanciaViaje) && !isNaN(precioCombustible) && !isNaN(rendimientoAuto)) {
             const litrosNecesarios = distanciaViaje / rendimientoAuto;
             const costoTotal = litrosNecesarios * precioCombustible;
 
-            viajes.push({
-                distancia: distanciaViaje,
-                precioLitro: precioCombustible,
-                rendimientoAuto: rendimientoAuto,
-                costoTotal: costoTotal
-            });
-
-            alert(`El costo total del combustible para tu viaje es de $${costoTotal.toFixed(2)}`);
+            alert(`El costo total del combustible para tu viaje es de $${costoTotal}`);
         } else {
             alert("Para ver el costo total de tu viaje debes ingresar los valores en números (sin letras, ni signos)");
         }
 
-        continuar = confirm("¿Deseas calcular otro viaje?");
-    } while (continuar);  
-
-    for (let i = 0; i < viajes.length; i++) {
-        const viajeIndividual = viajes[i];
-        console.log(`Viaje Nro. ${i + 1}:\nDistancia: ${viajeIndividual.distancia} km\nPrecio del litro: $${viajeIndividual.precioLitro.toFixed(2)}\nRendimiento de tu auto: ${viajeIndividual.rendimientoAuto.toFixed(2)} km/l\nCosto total: $${viajeIndividual.costoTotal.toFixed(2)}`);
-    }
+        nuevoCalculo = confirm("¿Deseas calcular otro viaje?");
+    } while (nuevoCalculo);
 }
 
-calcularCostoCombustible(); 
+calcularCostoCombustible();
